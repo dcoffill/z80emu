e851725f7a1138a604f3e73491c0f67b85e73cc1
dcoffill <dcoffill@cs.ucsb.edu>
2015-08-23 23:27:26 -0700

    Added doxygen comments to Registers

--- z80emu/src/Registers.cpp
+++ z80emu/src/Registers.cpp
@@ -53,25 +53,57 @@ void Registers::ex(const reg::DataReg16 reg)
 	(*_alt)[reg] = temp;
 }
 
+/**
+ * @brief Access or write general purpose registers in 16-bit mode
+ * @param regVal Register pair to access
+ *
+ * Access register pairs AF, BC, DE, and HL in 16-bit mode.
+ */
 uint16_t& Registers::operator[](const reg::DataReg16 regVal)
 {
 	return (*_main)[regVal];
 }
 
+/**
+ * @brief Access or write general purpose registers
+ * @param regVal Register to access
+ *
+ * Access general purpose registers B, C, D, E, F, H, and L.
+ */
 uint8_t& Registers::operator[](const reg::DataReg regVal)
 {
 	return (*_main)[regVal];
 }
 
+/**
+ * @brief Access or write special purpose 8-bit registers
+ * @param regVal Register to access
+ *
+ * Access the 8-bit special purpose registers I and R.
+ */
 uint8_t& Registers::operator[](const reg::Special8 regVal)
 {
 	return other[regVal.position];
 }
 
+/**
+ * @brief Access or write special purpose 16-bit registers
+ * @param regVal Register to access
+ *
+ * Access the 16-bit special purpose registers: IX, IY, PC, and SP.
+ */
 uint16_t& Registers::operator[](const reg::Special16 regVal)
 {
 	return shared16[regVal.position];
 }
+
+/**
+ * @brief Access status flag
+ * @param statusFlag Flag to access
+ *
+ * Access the status flags S, Z, H, P, N, and C.  This method performs bitwise arithmetic on the
+ * current "A" register and is guaranteed to contain the same flag values.
+ */
 
 uint8_t Registers::operator[](const flag::StatusFlag statusFlag)
 {
@@ -79,7 +111,13 @@ uint8_t Registers::operator[](const flag::StatusFlag statusFlag)
 	return (regF >> statusFlag) & 0x01; // shift bit in question to become new LSB, then mask to get its value
 }
 
-// set a flag to a given value (true or false)
+/**
+ * @brief Assign a status flag
+ * @param statusFlag Flag to set
+ * @param value Status to set
+ *
+ * Assign a status to the specified status flag
+ */
 void Registers::setFlag(const flag::StatusFlag statusFlag, bool value)
 {
 	(*this)[reg::F] &= ~(0x01 << statusFlag); // force that value to 0
